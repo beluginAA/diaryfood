e3417a232adf0dc28fc3f3978ff73a47d4dffd89
Белугин Александр Александрович <AlABelugin@rusas.local>
2023-04-11 13:42:05 +0300

    11.04.2023(main)

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-123
+import sys
+
+from loguru import logger
+
+
+logger.remove()
+mainLogger = logger.bind(name = 'mainLogger').opt(colors = True)
+mainLogger.add(sink = sys.stdout, format = '<green> {time:HH:MM:SS} </green> {message}', level = 'INFO')
+
+try:
+    for string in sys.stdin:
+        if string.strip('\n') == '':
+            sys.stdin.close()
+        else:
+            pass
+except Exception:
+    mainLogger.info('До встречи!')
